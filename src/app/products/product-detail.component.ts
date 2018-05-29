878f776d0146203637b7ef13e707bb4ddff1c882
Tom Millan <Tom.Millan@ifsautomation.com>
2018-05-29 10:52:35 -0400

    fixed url and guards

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -17,7 +17,7 @@ export class ProductDetailComponent implements OnInit {
     let id = +this._route.snapshot.paramMap.get('id');
     this.pageTitle += `: ${id}`;
     this.product = {
-      "productId": 1,
+        "productId": id,
         "productName": "Leaf Rake",
         "productCode": "GDN-0011",
         "releaseDate": "March 19, 2016",
